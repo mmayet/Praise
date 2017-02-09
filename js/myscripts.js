b4ed035c8e8ee2cac381466fbd0218f6a3d5f637
mmayet <mayetm93@gmail.com>
2017-02-09 14:12:30 -0800

    audiopoem

---
+++ js/myscripts.js
@@ -0,0 +1,4 @@
+var thousandPoemAudio = document.getElementById('thousandPoemAudio');
+thousandPoemAudio.addEventListener('play', function(e) {
+    ga('send', 'event', '1000Poem', 'Click', 'Audio');
+});
