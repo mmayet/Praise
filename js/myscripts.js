dbc725e272afd364b65ad3f14f607800c9491c03
mmayet <mayetm93@gmail.com>
2017-02-13 19:50:46 -0800

    fix js

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,7 +1,7 @@
-var thousandPoemAudio = document.getElementById('thousandPoemAudio');
+/*var thousandPoemAudio = document.getElementById('thousandPoemAudio');
 thousandPoemAudio.addEventListener('play', function(e) {
     ga('send', 'event', '1000Poem', 'Click', 'Audio');
-});
+});*/
 var specialMiraj = document.getElementById('specialMiraj');
 specialMiraj.addEventListener('play', function(e) {
     ga('send', 'event', 'specialMiraj', 'Click', 'Audio');
